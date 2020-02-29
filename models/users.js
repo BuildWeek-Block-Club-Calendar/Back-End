76e3bcf3a197d0d2020cf93e459a3aa3275460ea
Daniel Mattox <dmattox10@gmail.com>
2020-02-29 16:57:13 -0500

    Auth with JWT, routes for register and login, models for users and events

---
+++ models/users.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    streetAddress: {
+        type: String,
+        required: false
+    },
+    city: {
+        type: String,
+        required: false
+    },
+    zipcode: {
+        type: String,
+        required: false
+    },
+    businessName: {
+        type: String,
+        required: false
+    },
+    photo: {
+        data: Buffer,
+        contentType: String,
+        required: false
+    }                                                                  
+})
+
+const Users = mongoose.model('users', userSchema)
+
+module.exports = Users
