fddc094a8e3769a3395165f33fdff94aa2e1c0bf
Daniel Mattox <dmattox10@gmail.com>
2020-03-05 11:35:04 -0500

    Edited attend route

--- routes/rest.js
+++ routes/rest.js
@@ -129,7 +129,7 @@ router.post('/events/attend/:id', (req, res) => {
             }
         }, { new: true })
         .then(user => {
-            res.status(200).json(user)
+            res.status(200).json(event)
         })
         .catch(err => {
             res.status(500).json(err)
